5bbbb5fcd078d5b7c984d2dd46ca0462d5edf358
Yulij Andreevich Lesov <yalesov@gmail.com>
2016-07-06 16:47:10 +0800

    light refactoring

--- index.js
+++ index.js
@@ -1,61 +1,62 @@
-'use strict';
+'use strict'
 
-var util = require('util'),
-    growl = require('growl'),
-    path = require('path');
+var util = require('util')
+var growl = require('growl')
+var path = require('path')
 
-var MSG_SUCCESS = '%d tests passed in %s.',
-    MSG_FAILURE = '%d/%d tests failed in %s.',
-    MSG_ERROR = '';
+var MSG_SUCCESS = '%d tests passed in %s.'
+var MSG_FAILURE = '%d/%d tests failed in %s.'
+var MSG_ERROR = ''
 
 var OPTIONS = {
   success: {
     title: 'PASSED - %s',
-    image: path.join(__dirname, 'images/success.png')
+    image: path.join(__dirname, 'images', 'success.png')
   },
   failed: {
     title: 'FAILED - %s',
-    image: path.join(__dirname, 'images/failed.png')
+    image: path.join(__dirname, 'images', 'failed.png')
   },
   error: {
     title: 'ERROR - %s',
-    image: path.join(__dirname, 'images/error.png')
+    image: path.join(__dirname, 'images', 'error.png')
   }
-};
+}
 
+var GrowlReporter = function (helper, logger, config) {
+  var log = logger.create('reporter.growl')
 
-var GrowlReporter = function(helper, logger, config) {
-  var log = logger.create('reporter.growl');
+  var optionsFor = function (type, browser) {
+    var prefix = config && config.prefix ? config.prefix : ''
+    return helper.merge(OPTIONS[type], {
+      title: '' + prefix + util.format(OPTIONS[type].title, browser)
+    })
+  }
 
-  var optionsFor = function(type, browser) {
-    var prefix = config && config.prefix ? config.prefix : '';
-    return helper.merge(OPTIONS[type], {title: prefix + util.format(OPTIONS[type].title, browser)});
-  };
+  this.adapters = []
 
-  this.adapters = [];
-
-  this.onBrowserComplete = function(browser) {
-    var results = browser.lastResult;
-    var time = helper.formatTimeInterval(results.totalTime);
+  this.onBrowserComplete = function (browser) {
+    var results = browser.lastResult
+    var time = helper.formatTimeInterval(results.totalTime)
 
     if (results.disconnected || results.error) {
-      growl(MSG_ERROR, optionsFor('error', browser.name));
-      return;
+      growl(MSG_ERROR, optionsFor('error', browser.name))
+      return
     }
 
     if (results.failed) {
       growl(util.format(MSG_FAILURE, results.failed, results.total, time),
-        optionsFor('failed', browser.name));
-      return;
+        optionsFor('failed', browser.name))
+      return
     }
 
     growl(util.format(MSG_SUCCESS, results.success, time),
-      optionsFor('success', browser.name));
-  };
-};
+      optionsFor('success', browser.name))
+  }
+}
 
-GrowlReporter.$inject = ['helper', 'logger','config.growlReporter'];
+GrowlReporter.$inject = ['helper', 'logger', 'config.growlReporter']
 
 module.exports = {
   'reporter:growl': ['type', GrowlReporter]
-};
+}
